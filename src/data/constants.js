286975a0a6d825de11c2cff23496264e15828742
Bryce McWhirter <bryce.mcwhirter@paycomonline.com>
2024-09-30 06:35:11 -0500

    Work on the About Page.

--- src/data/constants.js
+++ src/data/constants.js
@@ -2,3 +2,6 @@ export const NAME = "Bryce McWhirter";
 export const SUBTITLE = "Software Developer By Day. Music Producer By Night.";
 export const NEWSLETTER_MSG = "I write a weekly newsletter where I share projects I'm currently working on along with 5 interesting things."
 
+
+// TAILWIND STYLES
+export const LINK_STYLE = "link text-primary"
